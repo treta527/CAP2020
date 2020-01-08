2be20f3c33e89aeb47edd48d613b12eae99703b5
leonardo <leonardo@leonardo-X550LN>
2020-01-08 10:47:31 -0500

    se agrega primera versión

---
+++ src/main/java/co/venko/poc/gpu/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.venko.poc.gpu;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Leonardo Machado Jaimes
+ */
+public class Main {
+
+    private static Logger logger = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+        logger.log(Level.INFO, "Hola Mundo!");
+    }
+}
